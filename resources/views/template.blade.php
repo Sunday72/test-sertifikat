841909f04288d28f23a6f8f1ec2911e84c07c8a9
Sunday72 <setyoputra2005@gmail.com>
2023-04-03 15:53:33 +0700

    Change Layout and Adding Nilai Table

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,69 +1,248 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Certificate</title>
+    <title>Sertifikat</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
     <style>
-        .an {
-            text-align: center
+        * {
+            margin: 0;
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
         }
 
-        .ttd {
-            position: absolute;
-            right: 20px;
-            bottom: 20px;
+        body {
+            background-image: url('img/bg-sertif.png');
+            background-size: cover;
+        }
+
+        #nama {
+            font-family: serif;
+        }
+
+        /* table */
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th,
+        td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
         }
     </style>
 </head>
 
 <body>
-    <div class="an">
-        <h1>SERTIFIKAT</h1>
+
+    {{-- LOGO --}}
+    <div class="text-center mt-5">
+        <img src="img/logo-unj.png" alt="Logo UNJ" width="100">
+        <img src="img/logo-pustikom.png" alt="Logo PUSTIKOM" width="100">
     </div>
-    <p>Manajemen Sekolah Menengah Kejuruan Negeri 12 Jakarta Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolorem quisquam numquam dicta?, menerangkan bahwa :</p>
-    <table>
-        <tr>
-            <td>Nama</td>
-            <td>:</td>
-            <td><b>{{ $nama }}</b></td>
-        </tr>
-        <tr>
-            <td>Sekolah</td>
-            <td>:</td>
-            <td><b>{{ $sekolah }}</b></td>
-        </tr>
-        <tr>
-            <td>Jurusan</td>
-            <td>:</td>
-            <td><b>{{ $jurusan }}</b></td>
-        </tr>
-    </table>
-    <p>Telah melaksanakan Praktek Kerja Lapangan di UP TIK UNJ dengan predikat <b>Sangat Baik.</b></p>
-    <div class="ttd">
-        <table>
-            <tr>
-                <td>Jakarta, 28 Maret 2023</td>
-            </tr>
-            <tr>
-                <td>Kepala UPT TIK,</td>
-            </tr>
-            <tr>
-                <td height="100"></td>
-            </tr>
-            <tr>
-                <td>
-                    <b>Dr. Uwes Anis Cheruman, M.Pd</b>
-                </td>
-            </tr>
-            <tr>
-                <td style="color: grey"><b>NIP.197403112002121001</b></td>
-            </tr>
+
+    {{-- TITLE --}}
+    <span class="text-center fw-bold d-block" style="font-size: 70px; color: #4F4F4F;">CERTIFICATE</span>
+
+    {{-- NAMA --}}
+    <div class="text-center">
+        <p class="mt-3" style="color: #1E1E1E;">diberikan kepada :</p>
+        <span id="nama" style="font-size: 50px;">Muhammad Aldi Tri Setiawan</span>
+    </div>
+    <div style="width: 700px; border-top: 2px solid #1E1E1E; margin: 16px auto;">
+        <div class="mt-2">
+            <span>SMK Negeri 12 Jakarta</span>
+            <span class="float-end">Rekayasa Perangkat Lunak</span>
+        </div>
+    </div>
+
+    {{-- PREDIKAT --}}
+    <div class="text-center mt-5">
+        <p>Telah melaksanakan Peraktek Kerja Lapangan di UPT TIK Universitas Negeri Jakarta dengan predikat <br>
+            <b class="mt-2 d-block">Sangat Baik</b>
+        </p>
+    </div>
+
+
+    {{-- TTD --}}
+    <div class="text-center mt-5">
+        <span class="d-block">Jakarta, 28 Maret 2023</span>
+        <span class="d-block">Kepala UPT TIK,</span>
+        <img src="img/ttd.png" alt="TTD" height="100px">
+        <span class="d-block">Dr. Uwes Anis Cheruman, M.Pd</span>
+        <span style="color: #505050">NIP.197403112002121001</span>
+    </div>
+
+
+
+
+    {{-- ------------------- PAGE 2 --------------------- --}}
+
+    {{-- TABEL NILAI --}}
+    <div class="container mt-5" style="page-break-before: always;">
+        <table align="center">
+            <thead style="background: #4FB1AD;">
+                <tr>
+                    <th rowspan="2">No</th>
+                    <th rowspan="2">Parameter</th>
+                    <th colspan="2">Nilai</th>
+                </tr>
+                <tr>
+                    <th>Angka</th>
+                    <th>Huruf</th>
+                </tr>
+            </thead>
+            <tbody style="background: #eaeaea">
+                {{-- A --}}
+                <tr>
+                    <th>A.</th>
+                    <th colspan="3">KEDISIPLINAN</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Ketepatan Waktu</td>
+                    <th>95</th>
+                    <th>A</th>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Tanggung Jawab Terhadap Tugas</td>
+                    <th>95</th>
+                    <th>A</th>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Kehadiran/Absensi</td>
+                    <th>95</th>
+                    <th>A</th>
+                </tr>
+                {{-- B --}}
+                <tr>
+                    <th>B.</th>
+                    <th colspan="3">PRESTASI KERJA</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Keterampilan Kerja</td>
+                    <th>95</th>
+                    <th>A</th>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Kualitas Hasil Kerja</td>
+                    <th>95</th>
+                    <th>A</th>
+                </tr>
+                {{-- C --}}
+                <tr>
+                    <th>C.</th>
+                    <th colspan="3">KEMAMPUAN BERADAPTASI</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Kemampuan Berkomunikasi</td>
+                    <th>95</th>
+                    <th>A</th>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Kerja Sama</td>
+                    <th>95</th>
+                    <th>A</th>
+                </tr>
+                {{-- D --}}
+                <tr>
+                    <th>D.</th>
+                    <th colspan="3">LAIN-LAIN</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Memiliki Rasa Percaya Diri</td>
+                    <th>95</th>
+                    <th>A</th>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Penampilan/Kerapihan</td>
+                    <th>95</th>
+                    <th>A</th>
+                </tr>
+            </tbody>
         </table>
+        <div class="row mt-4">
+            <div style="float: right;">
+                <table align="center">
+                    <thead style="background: #4FB1AD;">
+                        <tr>
+                            <th>TOTAL NILAI</th>
+                            <th>RATA-RATA</th>
+                            <th>HURUF</th>
+                        </tr>
+                    </thead>
+                    <tbody style="background: #eaeaea">
+                        <tr>
+                            <td>836</td>
+                            <td>92.88</td>
+                            <th>A</th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <label>Ketentuan Penilaian</label>
+                <ol type="1">
+                    <li>Nilai 80 - 100 = A</li>
+                    <li>Nilai 68 - 79 = B</li>
+                    <li>Nilai 50 - 67 = C</li>
+                </ol>
+            </div>
+        </div>
     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+        integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
